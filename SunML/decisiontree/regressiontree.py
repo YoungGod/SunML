1b6334608cb5be6f43fbac87610780287f432b0a
JieYang <dearyangjie@yeah.net>
2017-05-14 23:55:52 +0800

    regressiontree.py

---
+++ SunML/decisiontree/regressiontree.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 22:33:02 2017
+
+@author: Young
+"""
+
+import numpy as np
+
+class RegressionTree:
+    """
+    re_tree: 回归树
+    """
+    def __init__(self):
+
+        self.tree = None    
+   
+    def bin_split(self, dataset, feat, value):
+        right_set = dataset[np.where(dataset[:,feat] > value)]
+        left_set = dataset[np.where(dataset[:,feat] <= value)]
+        return right_set, left_set
+    
+    def err_leaf(self, dataset_y):
+        pass
+    
+    def choose_best_split(self, dataset, tol_m, tol_s):
+        """
+        功能：中断条件(1) 划分数据集的大小达到限定值
+             选择条件：划分后数据集误差减少最大的feature及value
+        """
+        if dataset[:,-1].std() == 0.0:
+            return None, dataset[:,-1].mean()
+        
+        err = dataset.shape[0]*np.var(dataset[:,-1])
+        best_err = np.inf
+        num_feat = dataset.shape[1] - 1
+        best_feat = 0; best_value = dataset[0,0]
+        for i_feat in xrange(num_feat):
+            for value in dataset[:,i_feat]:
+                right_set, left_set = self.bin_split(dataset, i_feat, value)
+                if right_set.shape[0] < 1 or left_set.shape[0] < 1:  # 确保划分数据集不为空
+                    continue
+                new_err = right_set.shape[0]*np.var(right_set[:,-1]) + left_set.shape[0]*np.var(left_set[:,-1])
+                if new_err < best_err:
+                    best_feat = i_feat
+                    best_value = value
+                    best_err = new_err
+                    
+        right_set, left_set = self.bin_split(dataset, tol_m, tol_s)
+        if right_set.shape[0] < tol_m and left_set.shape[0] < tol_m:
+            return None, dataset[:,-1].mean()
+        if err - best_err < tol_s:
+            return None, dataset[:,-1].mean()
+        
+        return best_feat, best_value
+    
+    def create_tree(self, dataset, tol_m, tol_s):
+        """
+        功能：创建一个回归树
+        """
+        best_feat, best_value =  self.choose_best_split(dataset, tol_m, tol_s)
+        
+        if best_feat == None:    # 通过choose_best_split函数返回值来创建终止条件
+            return best_value
+        
+        tree = {}
+        right_set, left_set = self.bin_split(dataset, best_feat, best_value)
+        tree['feature'] = best_feat; tree['value'] = best_value                  
+        tree['right'] =  self.create_tree(right_set, tol_m, tol_s)
+        tree['left'] = self.create_tree(left_set, tol_m, tol_s)
+        
+        return tree
+    
+    def train(self, X, y, tol_m = 2, tol_s = 0.0):
+#        if len(np.unique(y)) == 1:
+        if y.std() == 0.0:
+            self.tree = y[0]
+        else:
+#            print "train1 OK"
+            dataset = np.concatenate((X,y.reshape(-1,1)),axis=1)
+#            print "trian2 OK"
+            self.tree = self.create_tree(dataset, tol_m, tol_s)
+
+    def _pred(self, x, tree):
+        """
+        """
+        feat = tree['feature']
+        if x[feat] > tree['value']:
+            if tree['right'] is dict:
+                self._pred(x, tree['right'])
+            else:
+                return tree['right']
+            
+        if x[feat] <= tree['value']:
+            if tree['left'] is dict:
+                self._pred(x, tree['left'])
+            else:
+                return tree['left']
+    
+    def predict(self, X):
+        tree = self.tree
+        y_pred = []
+        for x in X:
+            y_pred.append(self._pred(x, tree))
+        return np.array(y_pred)
+    
+if __name__ == "__main__":
+    np.random.seed(0)
+    X = np.random.random((10,4))
+    y = np.random.randint(0,3,10)
+    re_tree = RegressionTree()
+    re_tree.train(X,y)
+    print re_tree.tree
+    
+    # Fitting
+    
+    fr = open('data.txt','r')
+    data_set = []
+    for line in fr.readlines():
+        data_set.append([float(i) for i in line.strip().split()])
+   
+    data_set = np.array(data_set)
+    X = data_set[:,0:-1] 
+    y = data_set[:,-1]
+    
+    re_tree = RegressionTree()
+    re_tree.train(X,y)
+    
+#    pred = predict(re_tree, data_set[0,0:-1])
+    
+    pred = re_tree.predict(X)
+    print "OK"
+    
+    import matplotlib.pyplot as plt
+    plt.scatter(data_set[:,1],data_set[:,2],c='b',label = "Origin")
+    plt.scatter(data_set[:,1],pred,c='r',label = "Fit")
+    plt.legend()
+    plt.show()
+    
+    pred = pred[data_set[:,1].argsort(),:]
+    data_set = data_set[data_set[:,1].argsort(),:]
+    plt.plot(data_set[:,1],data_set[:,2],'b-',label = "Origin")
+    plt.plot(data_set[:,1],pred,'r-.',label = "Fit")
+    plt.legend()
+    plt.show()
+
+#    # testing
+#    test_data = load_dataset('ex1.txt')
+#    pred = np.zeros((len(test_data),1))
+#    for i in xrange(len(test_data)):
+#        pred[i,:] = predict(re_tree, test_data[i,0:-1])    
+#    err_no_prune = sum(np.power(test_data[:,-1] - pred.flatten(),2))
+#    
+#    pred = pred[test_data[:,1].argsort(),:]
+#    test_data = test_data[test_data[:,1].argsort(),:]
+#    plt.plot(test_data[:,1],test_data[:,2],'b-',label = "Origin")
+#    plt.plot(test_data[:,1],pred,'r-.',label = "Predict")
+#    plt.legend()
+#    plt.show()   
---
+++ tests/decisontree_test.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 22:57:05 2017
+
+@author: Young
+"""
+
+
