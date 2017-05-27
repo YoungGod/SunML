e001f4792445ea2dbe1fed9a4594ed341afd5d31
JieYang <dearyangjie@yeah.net>
2017-05-27 15:40:04 +0800

    class linear regression

---
+++ SunML/regression/linearregression.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 27 15:37:35 2017
+
+@author: Young
+"""
+
+class LinearRegression:
+    """
+    """
+    def __init__(self):
+        pass
+    
+    pass
+
+if __name__ == '__main__':
+    pass
