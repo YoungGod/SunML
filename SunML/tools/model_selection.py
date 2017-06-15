d85e4dd657fc3bb7cff6c0d32f8dec8110ca0243
JieYang <dearyangjie@yeah.net>
2017-06-15 15:56:57 +0800

    debug

---
+++ SunML/tools/model_selection.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 14 16:02:02 2017
+
+@author: Young
+"""
+
--- model_selection.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 14 15:56:30 2017
-
-@author: Young
-"""
-
-class cv:
-    pass
